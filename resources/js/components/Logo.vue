f3ac7b7374b8b94820511ba3780461a7572e49ca
Pedro Padron <padronpedro@gmail.com>
2020-09-05 15:55:57 -0400

    Signifly Logo added as svg

---
+++ resources/js/components/Logo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <img :src="mySVG" width="70%">
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+    return {
+      mySVG: require('../assets/signifly.svg')
+    }
+  }
+}
+</script>
