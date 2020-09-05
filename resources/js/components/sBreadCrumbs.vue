c16994bcb71fd82db31f6b270ad45fa8ab868780
Pedro Padron <padronpedro@gmail.com>
2020-09-05 16:06:50 -0400

    Custom breadcrumbs for the admin module with name and link

---
+++ resources/js/components/sBreadCrumbs.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="bread-main">
+        <div v-for="(oneItem, index) in listLink" :key="index">
+            <a @click.prevent="$goRouter(oneItem.link)" v-if="oneItem.link">
+                {{oneItem.text}}
+            </a>
+            <span v-else>{{oneItem.text}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        listLink: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.bread-main{
+    margin-top: 20px;
+    margin-left: 10px;
+}
+.bread-main a {
+    cursor: pointer;
+    color: black;
+}
+.bread-main a:hover {
+    color: gray;
+}
+.bread-main div:not(larst-child) a:after {
+  padding-left: 10px;
+  content: "/";
+}
+.bread-main div {
+    display: inline-block;
+    margin-right: 20px;
+}
+</style>
