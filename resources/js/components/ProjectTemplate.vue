db249442e6cac141f52601c289a70cf9aea06453
Pedro Padron <padronpedro@gmail.com>
2020-09-05 18:07:55 -0400

    Template to display project information ready

--- resources/js/components/ProjectTemplate.vue
+++ resources/js/components/ProjectTemplate.vue
@@ -1,13 +1,55 @@
 <template>
     <div>
-        <div>{{name}}</div>
+        <div>
+            <div class="title-form">
+                Project details
+            </div>
+             <span>
+                Thank you for being part of our distinguished clients. Below you will find the detailed and updated information of your project. If you have any questions, do not hesitate to contact us.
+            </span>
+        </div>
+        <div class="project-name">{{name}}</div>
+        <div class="project-client">{{client ? client.name : ''}}</div>
         <div>{{description}}</div>
-        <div>{{start_date}}</div>
-        <div>{{end_date}}</div>
-        <div>{{budget}}</div>
+        <div class="project-constraints">Constraints</div>
         <div>{{constraints}}</div>
-        <div>{{final_cost}}</div>
-        <div>{{client}}</div>
+        <div class="project-constraints">Estimation</div>
+        <div class="project-estimation">
+            <div>
+                <div>
+                    {{start_date ? start_date: 'TBD'}}
+                </div>
+                <div class="project-subtitle">
+                    Start date
+                </div>
+            </div>
+            <div>
+                <div>
+                    {{end_date ? end_date: 'TBD'}}
+                </div>
+                <div class="project-subtitle">
+                    End date
+                </div>
+            </div>
+            <div>
+                <div>
+                    CAD {{budget ? budget: 'TBD'}}
+                </div>
+                <div class="project-subtitle">
+                    Budget
+                </div>
+            </div>
+            <div>
+                <div>
+                    CAD {{final_cost ? final_cost: 'TBD'}}
+                </div>
+                <div class="project-subtitle">
+                    Final cost
+                </div>
+            </div>
+        </div>
+        <div class="project-constraints">Project team</div>
+
         <div v-for="(oneItem,index) in members" :key="index" class="pic-team-list">
             <s-team-card
                 :name="oneItem.name"
@@ -64,12 +106,45 @@ export default {
             type: Array,
             required: false
         }
-    }
+    },
 }
 </script>
 <style scoped>
 .pic-team-list {
     display: inline-block;
-
+}
+.project-name {
+    font-size: 30px;
+    text-transform: uppercase;
+    line-height: 70px;
+    margin-top: 30px;
+    font-family: 'Anton', sans-serif;
+    color: black;
+}
+.project-client {
+    font-size: 18px;
+    text-transform: uppercase;
+    line-height: 70px;
+    margin-bottom: 4px;
+    font-family: 'Anton', sans-serif;
+    color: black;
+}
+.project-constraints{
+    font-size: 14px;
+    line-height: 70px;
+    margin-bottom: 4px;
+    font-family: 'Anton', sans-serif;
+    color: black;
+}
+.project-estimation {
+    display: flex;
+}
+.project-estimation > div {
+    flex: 20%;
+    text-align: center;
+}
+.project-subtitle{
+    font-size: 12px;
+    font-weight: bold;
 }
 </style>
