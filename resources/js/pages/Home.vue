337843bed86289c96f7de6de53a85f8c5b958d6f
Pedro Padron <padronpedro@gmail.com>
2020-09-05 16:12:00 -0400

    Reorganizing layout

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,25 +1,67 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
+    <div>
+        <div>
+            <div class="title-form">
+                Project DEMO
+            </div>
+        </div>
 
-                    <div class="card-body">
-                        I'm an example component...---
-                    </div>
-                    <button @click="$goRouter('second')">Go to second</button>
-                    <button @click="$goRouter('admin.home')">Admin</button>
-                </div>
+        <div class="title-info">
+            <span>
+                Please select an option to continue
+            </span>
+        </div>
+
+        <div class="flex-container">
+            <div class="flex-container-col">
+                <s-button :buttonText="'Project request'" @clickAction="$goRouter('client.project.request')"></s-button>
+            </div>
+            <div class="flex-container-col">
+                <s-button :buttonText="'Administrator'" @clickAction="$goRouter('admin.home')"></s-button>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
+export default {
+    data () {
+        return {
         }
+    },
+    methods: {
     }
+}
 </script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Anton&display=swap');
+.flex-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.flex-container-col {
+  flex: 25%;
+  margin: 10px;
+  text-align: center;
+  font-size: 30px;
+}
+
+@media screen and (max-width: 992px) {
+  .flex-container-col {
+    flex: 50%;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .flex-container-col {
+    flex-direction: column;
+  }
+}
+.pic-team-list {
+    display: inline-block;
+
+}
+
+</style>
--- resources/js/pages/admin/Home.vue
+++ resources/js/pages/admin/Home.vue
@@ -1,8 +1,12 @@
 <template>
     <div>
         <Menu />
-        <div>
-            <p>Please select an option from the menu</p>
+
+        <div class="help-menu">
+            Select an option from the menu
+        </div>
+        <div class="help-menu">
+            <s-button :buttonText="'Log out'" @clickAction="$goRouter('home')"></s-button>
         </div>
     </div>
 </template>
@@ -11,3 +15,40 @@
     export default {
     }
 </script>
+
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Anton&display=swap');
+.flex-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.flex-container-col {
+  flex: 25%;
+  margin: 10px;
+  text-align: center;
+  font-size: 30px;
+}
+
+@media screen and (max-width: 992px) {
+  .flex-container-col {
+    flex: 50%;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .flex-container-col {
+    flex-direction: column;
+  }
+}
+.pic-team-list {
+    display: inline-block;
+
+}
+.help-menu{
+    margin-top:100px;
+    text-align: center;
+    font-weight: bold;
+}
+</style>
