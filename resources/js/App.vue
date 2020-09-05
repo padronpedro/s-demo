391a65d77e9257dd732cedfd95c9d2e00fd1c1e0
Pedro Padron <padronpedro@gmail.com>
2020-09-05 18:50:18 -0400

    Added loading table in Adm(Client, member, list) component

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -49,6 +49,11 @@
 </script>
 
 <style>
+.loadingDatatable {
+    font-weight: bold;
+    font-size: 20px;
+    text-align: center;
+}
 .box-main-content {
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
   transition: 0.3s;
--- resources/js/pages/admin/members/AdmMembers.vue
+++ resources/js/pages/admin/members/AdmMembers.vue
@@ -7,7 +7,10 @@
                 <s-button :buttonText="'Add a new member'" @clickAction="clickAddMember" style="float:right"></s-button>
             </div>
 
-            <p v-if="dataTable.length===0">No clients found</p>
+            <p v-if="(dataTable.length===0) && !loadingData">No clients found</p>
+            <div v-if="loadingData" class="loadingDatatable">
+                Loading members list....
+            </div>
 
             <div class="tabBox" v-if="dataTable.length>0">
                 <table>
@@ -47,6 +50,7 @@
   export default {
     data () {
       return {
+          loadingData: false,
           dataTable: [],
           dataTableHeader: [
               { name: 'Name' },
@@ -74,8 +78,10 @@
         this.$goRouter('admin.members.add')
       },
       getDataTable () {
+          this.loadingData = true
           axios.get('/api/v1/members', {})
             .then(response => {
+                this.loadingData = false
                 let info = response.data
                 if(info.status === 'SUCCESS') {
                     this.dataTable = info.data
@@ -84,6 +90,7 @@
                 }
             })
             .catch(error => {
+                this.loadingData = false
                 this.$refs.snackbar.showSnack(('Error getting members data') + ': ' + error)
             })
       },
