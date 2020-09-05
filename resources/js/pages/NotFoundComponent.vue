f6994109e6819fb83707d281ba324e6a1b5a7810
Pedro Padron <padronpedro@gmail.com>
2020-09-05 18:06:47 -0400

    404 component

--- resources/js/pages/NotFoundComponent.vue
+++ resources/js/pages/NotFoundComponent.vue
@@ -1,11 +1,28 @@
 <template>
     <div>
-        not found
+        <div>
+            <div class="title-form">
+                We didn't found this page
+            </div>
+            <div class="button-contact">
+                <s-button :buttonText="'Go to Home'" @clickAction="$goRouter('home')"></s-button>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
 
+export default {
+    data () {
+        return {
+        }
+    },
 }
 </script>
+
+<style scoped>
+.button-contact {
+    text-align: center;
+}
+</style>
